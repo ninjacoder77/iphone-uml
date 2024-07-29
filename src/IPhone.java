2dc85984e9691b35ce8930f9296f579fd8241095
ninjacoder77 <ps4nenao@gmail.com>
2024-07-29 15:58:16 -0300

    enviando projeto

--- src/IPhone.java
+++ src/IPhone.java
@@ -1,5 +1,47 @@
-public class IPhone {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class IPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada");
+    }
+
+    @Override
+    public void iniciarCorreioPorVoz() {
+        System.out.println("INiciando correio de voz");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("exibindo pagina:  " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando a pagina");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("REproduzindo musica");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Musica pausada");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Musica selecionada: " + musica);
     }
 }
